f10967b85920740c3841c991d07da784bc23c470
Rock <0123956@kdu-online.com>
2020-08-28 16:44:17 +0800

    Created new sln

--- Mario/LuigiEnemyActor.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "EnemyActor.h"
-
-class LuigiEnemyActor : public EnemyActor
-{
-public:
-	LuigiEnemyActor(std::vector<GameObject*>* mpEntities);
-};
--- Mario/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "Game.h"
-
-int main(int argc, char* args[])
-{
-	Game game;
-	game.Run();
-	return 0;
-}
